c4430eb8d69ed6d2860bb7d09083910763b109f9
shubh <shubhamkush012@gmail.com>
2024-02-17 23:20:20 +0530

    .

--- client/cryptogate/src/components/Welcome.jsx
+++ client/cryptogate/src/components/Welcome.jsx
@@ -9,9 +9,15 @@ const ConnectWallet =() =>{
 
 }
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
-const Input = ({Placeholder}) =>(
-  <Input 
+const Input = ({Placeholder,name, type,value,handleChange}) =>(
+  <input 
   Placeholder={Placeholder}
+  type ={type}
+  step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  
   />
 
 );
@@ -76,7 +82,7 @@ const Welcome =() => {
 
             </div>
             <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-              <Input Placeholder="Adressto" name="adress to" type="text" handleChange ={() =>{}} />
+            <Input Placeholder="Address To "nmae="addressTo" type="text"handleChange={() =>{}}/>
             </div>
 
           </div>
